f5ef4eb1cefff9b4d40de13d31abd25704d5be18
Vishal Ranjan <vishivish18@gmail.com>
2016-02-18 00:02:35 +0530

    fixing diagonal logic, sum initialized to zero in else

--- public/app/controllers/gameCtrl.js
+++ public/app/controllers/gameCtrl.js
@@ -77,6 +77,7 @@ angular.module('app')
                                     alert("Winner is " + candidate)
                                 } else {
                                     //now check the left diagonal
+                                    sum = 0
                                     for (var i = 0; i < arr.length; i++) {
                                         for (var j = 0; j < arr.length; j++) {
                                             if (i == j) {
@@ -89,6 +90,7 @@ angular.module('app')
                                         alert("Winner is " + candidate)
                                     } else {
                                         //now check the right diagonal
+                                        sum = 0
                                         for (var i = 0; i < arr.length; i++) {
                                             for (var j = 0; j < arr.length; j++) {
                                                 if (i + j == 2) {
