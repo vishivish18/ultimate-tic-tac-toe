550dcce686c985a63b7a371f4bf3df0704254e10
Vishal Ranjan <vishivish18@gmail.com>
2016-02-22 00:03:41 +0530

    messy but logic working, now cleaning required

--- public/app/controllers/gameCtrl.js
+++ public/app/controllers/gameCtrl.js
@@ -4,6 +4,7 @@ angular.module('app')
         var socket = io();
         var id = $stateParams.id;
         $scope.status = "Waiting for players"
+        $scope.player = ""
         $scope.setup = function() {
             // on connection to server send the id of games url
             socket.on('connect', function() {
@@ -11,40 +12,51 @@ angular.module('app')
             });
 
 
+            socket.on('setplayer', function(data) {
+                console.log("setplayer emitted", data)
+                $scope.player = data.player
+            })
+
 
             // receive the names and avatars of all people in the chat room
             socket.on('peopleinchat', function(data) {
-
+                console.log("peopleinchat triggered from backend")
                 if (data.number === 0) {
 
                     console.log("data.number is 0")
-
-                    // call the server-side function 'login' and send user's parameters                    
+                    localStorage.setItem("PLayer", "X");
+                    // call the server-side function 'login' and send user's parameters  
                     socket.emit('login', {
                         user: $rootScope.currentUser,
                         id: id
                     });
+
 
                 } else if (data.number === 1) {
                     console.log("data.number is 1")
                     console.log(data)
                     $scope.status = "Player Joined"
-                    $scope.$apply()
+                    $scope.turn = true;
+                    $scope.setup()
                     $scope.chatWith = data.user;
                     console.log($scope.chatWith)
                     $scope.name = data.user
                     console.log("scope name is" + $scope.name)
-
                     socket.emit('login', {
                         user: $rootScope.currentUser,
                         id: id
                     });
+
 
                 } else {
                     console.log("Chat is full")
                 }
 
             });
+
+
+
+
             // Other useful 
 
 
@@ -59,12 +71,34 @@ angular.module('app')
 
             });
 
+            /* socket.on('receive', function(data) {                        
+                 console.log(data)
+                 if(data.user == "X"){
+                     $scope.Values[data.x][data.y] = 'X'
+                     xValues[data.x][data.y] = 1;
+                     $scope.turn = !$scope.turn;
+                 }else{
+                     $scope.Values[data.x][data.y] = '0'
+                     yValues[data.x][data.y] = 1;
+                     $scope.turn = !$scope.turn;
+                 }                
+
+                  
+                 
+             });*/
+            socket.on('receive', function(data) {
+
+                console.log("Recieved by me")
+                console.log(data);
+                
+            });
+
         }
 
         $scope.setup();
 
 
-        $scope.turn = 1
+        $scope.turn = -1
 
 
         $scope.Values = [
@@ -180,11 +214,29 @@ angular.module('app')
             }
         }
 
+        
+         socket.on('rtest', function(data) {
+
+                console.log("Recieved by me in test")
+                console.log(data);               
+                   if(data.user == "X"){
+                     $scope.Values[data.x][data.y] = 'X'
+                     xValues[data.x][data.y] = 1;
+                     $scope.turn = !$scope.turn;
+                 }else{
+                     $scope.Values[data.x][data.y] = '0'
+                     yValues[data.x][data.y] = 1;
+                     $scope.turn = !$scope.turn;
+                 }                
+
+                
+            });
 
         $scope.play = function(x, y) {
+            
             console.log("game on")
             if ($scope.Values[x][y] == '') {
-                if ($scope.turn == 1) {
+                if ($scope.turn == true && $scope.player == 'X') {
                     $scope.Values[x][y] = 'X'
                     xValues[x][y] = 1;
                     socket.emit('move', {
@@ -192,9 +244,10 @@ angular.module('app')
                         x: x,
                         y: y
                     });
+
                     $scope.winner(xValues, 'X');
                     $scope.turn = !$scope.turn;
-                } else {
+                } else if ($scope.turn == false && $scope.player == '0') {
                     $scope.Values[x][y] = '0'
                     yValues[x][y] = 1;
                     socket.emit('move', {
@@ -204,6 +257,10 @@ angular.module('app')
                     });
                     $scope.winner(yValues, '0');
                     $scope.turn = !$scope.turn;
+                } else {
+                    console.log("Not enough players")
+                    console.log("Player is" + $scope.player)
+                    console.log("turn of" + $scope.turn)
                 }
             } else {
                 console.log("taken")
--- routes.js
+++ routes.js
@@ -1,146 +1,150 @@
-module.exports = function(app,io){
-	
-	console.log("hello");
-	// Initialize a new socket.io application, named 'chat'
-	var chat = io.on('connection', function (socket) {
-		
-		socket.on('load',function(data){
-			
-			//client sends the games url in data
-			console.log("data from the client"+data)			
-
-			var room = findClientsSocket(io,data);
-			console.log(room.length)
-			if(room.length === 0 ) {
-
-				socket.emit('peopleinchat', {number: 0});
-			}
-			else if(room.length === 1) {
-				console.log("now room length is 1")			
-				socket.emit('peopleinchat', {
-					number: 1,
-					user: room[0].username,					
-					id: data
-				});
-			}
-			else if(room.length >= 2) {
-
-				chat.emit('tooMany', {boolean: true});
-			}
-		});
-
-		// When the client emits 'login', save his name and avatar,
-		// and add them to the room
-
-		socket.on('move',function(data){
-			console.log("the move is "+data.x + data.y + 'by player'+data.user )
-
-		})
+module.exports = function(app, io) {
 
 
-		socket.on('login', function(data) {
+    // Initialize a new socket.io application, named 'chat'
+    var chat = io.on('connection', function(socket) {
 
-			console.log("login action triggered")
-		
-			var room = findClientsSocket(io, data.id);
-		
-			// Only two people per room are allowed
-			if (room.length < 2) {
+        socket.on('load', function(data) {
 
-				console.log("room length is less than 2")
-				// Use the socket object to store data. Each client gets
-				// their own unique socket object
-
-				socket.username = data.user;
-				socket.room = data.id;
-				
-
-				// Tell the person what he should use for an avatar
-				socket.emit('img', socket.avatar);
+            //client sends the games url in data
 
 
-				// Add the client to the room
-				socket.join(data.id);
+            var room = findClientsSocket(io, data);
 
-				console.log("room length again"+ room.length);
+            if (room.length === 0) {
+                //make the first player = X;
 
-				if (room.length == 1) {
-					console.log("room length is 1")
-					var usernames = [],
-						avatars = [];
+                io.sockets.emit('peopleinchat', { number: 0 });
 
-					usernames.push(room[0].username);
-					usernames.push(socket.username);
-
-					avatars.push(room[0].avatar);
-					avatars.push(socket.avatar);
-
-					// Send the startChat event to all the people in the
-					// room, along with a list of people that are in it.
-
-					chat.in(data.id).emit('startChat', {
-						boolean: true,
-						id: data.id,
-						users: usernames,
-						avatars: avatars
-					});
-				}
-			}
-			else {
-				socket.emit('tooMany', {boolean: true});
-			}
-		});
-
-		// Somebody left the chat
-		socket.on('disconnect', function() {
-
-			// Notify the other person in the chat room
-			// that his partner has left
-
-			socket.broadcast.to(this.room).emit('leave', {
-				boolean: true,
-				room: this.room,
-				user: this.username,
-				avatar: this.avatar
-			});
-
-			// leave the room
-			socket.leave(socket.room);
-		});
+                socket.emit('setplayer', { player: 'X' });
 
 
-		// Handle the sending of messages
-		socket.on('msg', function(data){
-			console.log("A new message recieved")
-			console.log(data)
-			// When the server receives a message, it sends it to the other person in the room.
-			socket.broadcast.to(socket.room).emit('receive', {msg: data.msg, user: data.user});
-		});
-	});
+            } else if (room.length === 1) {
+
+                socket.emit('setplayer', { player: '0' });
+
+                io.sockets.emit('peopleinchat', {
+                    number: 1,
+                    user: room[0].username,
+                    id: data
+                });
+            } else if (room.length >= 2) {
+
+                chat.emit('tooMany', { boolean: true });
+            }
+        });
+
+        // When the client emits 'login', save his name and avatar,
+        // and add them to the room
+
+
+
+
+        socket.on('login', function(data) {
+
+
+            var room = findClientsSocket(io, data.id);
+
+            // Only two people per room are allowed
+            if (room.length < 2) {
+
+
+                // Use the socket object to store data. Each client gets
+                // their own unique socket object
+
+                socket.username = data.user;
+                socket.room = data.id;
+
+
+                // Tell the person what he should use for an avatar
+                socket.emit('img', socket.avatar);
+
+
+                // Add the client to the room
+                socket.join(data.id);
+
+
+
+                if (room.length == 1) {
+
+                    var usernames = [],
+                        avatars = [];
+
+                    usernames.push(room[0].username);
+                    usernames.push(socket.username);
+
+                    avatars.push(room[0].avatar);
+                    avatars.push(socket.avatar);
+
+                    // Send the startChat event to all the people in the
+                    // room, along with a list of people that are in it.
+
+                    chat.in(data.id).emit('startChat', {
+                        boolean: true,
+                        id: data.id,
+                        users: usernames,
+                        avatars: avatars
+                    });
+                }
+            } else {
+                socket.emit('tooMany', { boolean: true });
+            }
+        });
+        // Somebody left the chat
+        socket.on('disconnect', function() {
+
+            // Notify the other person in the chat room
+            // that his partner has left
+
+            socket.broadcast.to(this.room).emit('leave', {
+                boolean: true,
+                room: this.room,
+                user: this.username,
+                avatar: this.avatar
+            });
+
+            // leave the room
+            socket.leave(socket.room);
+        });
+
+
+        // Handle the sending of messages
+        /* socket.on('move', function(data) {
+             console.log("A new move recieved")
+             console.log(data)
+                 // When the server receives a message, it sends it to the other person in the room.
+            	socket.broadcast.to(socket.room).emit('receive', { user: data.user, x: data.x, y: data.y });
+             socket.broadcast`.emit('receive', { user: data.user, x: data.x, y: data.y });
+         });*/
+        socket.on('move', function(data) {
+            console.log("A new message recieved")
+            console.log(data)
+                // When the server receives a message, it sends it to the other person in the room.
+            socket.broadcast.to(socket.room).emit('rtest', { user: data.user, x: data.x, y: data.y });
+
+        });
+
+    });
 };
 
-function findClientsSocket(io,roomId, namespace) {
-	console.log("in client socket finding function")
-	console.log(roomId,namespace);
-	var res = [],
-		ns = io.of(namespace ||"/");    // the default namespace is "/"
+function findClientsSocket(io, roomId, namespace) {
 
-	if (ns) {
-		console.log("if condition executed");		
-		for (var id in ns.connected) {			
+    var res = [],
+        ns = io.of(namespace || "/"); // the default namespace is "/"
 
-			if(roomId) {
-				var index = ns.connected[id].rooms.indexOf(roomId) ;
-				if(index !== -1) {
-					res.push(ns.connected[id]);
-				}
-			}
-			else {
-				res.push(ns.connected[id]);
-			}
-		}
-	}
-	return res;
+    if (ns) {
+
+        for (var id in ns.connected) {
+
+            if (roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId);
+                if (index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
 }
-
-
